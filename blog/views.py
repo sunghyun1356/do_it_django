5c9e85d53694b276384c8caab7f24c2e1e04d1aa
sunghyun1356 <sunghyun1356@naver.com>
2023-02-28 13:36:27 +0900

    post list page bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 # Create your views here.
 
@@ -7,24 +6,27 @@ class PostList(ListView):
     model = Post
     ordering = '-pk'
 
+class PostDetail(DetailView):
+    model = Post
+
 #def index(request):
 #    posts = Post.objects.all().order_by('-pk')
 
 #    return render(
 #        request,
-#       'blog/post_index.html',
+#       'blog/post_list.html',
 #        {
 #            'posts': posts,
 #       }
 #    )
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
+#def single_post_page(request, pk):
+#    post = Post.objects.get(pk=pk)
 
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+#    return render(
+#        request,
+#        'blog/post_detail.html',
+#        {
+#            'post': post,
+#        }
+#    )
